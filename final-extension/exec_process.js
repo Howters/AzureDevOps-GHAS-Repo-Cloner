1e8d143eecb37c9e31318437e28b41dbedffed86
Hans Khomulia <hans.khomulia@binus.edu>
2024-05-10 22:40:47 +0700

    Fix: Fix exec_process maxbuffer

--- final-extension/exec_process.js
+++ final-extension/exec_process.js
@@ -3,6 +3,12 @@ var exec = require('child_process').exec;
 var result = function(command, cb){ // CHANGE THE VALUE OF MAXBUFFER IF ERROR STDOUT
     var child = exec(command, {maxBuffer: 1024 * 800}, function(err, stdout, stderr){
         if(err != null){
+            return cb(new Error(err), null);
+        }else if(typeof(stderr) != "string"){
+            return cb(new Error(stderr), null);
+        }else{
+            return cb(null, stdout);
+        }
     });
 }
 
